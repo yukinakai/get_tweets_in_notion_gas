ebfcb02d2cba7cea88bb27d9439ce5dee6f99b5e
yukinakai <10libra20.y@gmail.com>
2022-08-27 09:24:06 +0900

    get tweets in notion

--- main.ts
+++ main.ts
@@ -1,3 +1,3 @@
 function main() {
-  console.log(notion_api_header())
+  getTweetsInNotion()
 }
--- src/get_tweets_in_notion.ts
+++ src/get_tweets_in_notion.ts
@@ -1,4 +1,25 @@
-function get_tweets_in_notion() {
+function getTweetsInNotion() {
   const NOTION_DATABASE_ID: string = PropertiesService.getScriptProperties().getProperty("NOTION_DATABASE_ID");
-  const url: string = 'https://api.notion.com/v1/databases/' + NOTION_DATABASE_ID + '/query'
+  const url: string = 'https://api.notion.com/v1/databases/' + NOTION_DATABASE_ID + '/query';
+  const payload: { [name: string]: number } = {'page_size': 100};
+  let has_more: boolean = true;
+  const tweets_in_notion = []
+  while(has_more) {
+    const res = JSON.parse(UrlFetchApp.fetch(url, getTweetsOptions(payload)).getContentText());
+    for(let result of res['results']) {
+      tweets_in_notion.push(result['properties']['Tweet_id']['rich_text'][0]['plain_text'])
+    }
+    has_more = res['has_more']
+    payload['start_cursor'] = res['next_cursor']
+  }
+  return tweets_in_notion
+}
+
+function getTweetsOptions(payload: { [name: string]: number|string }) {
+  const options: GoogleAppsScript.URL_Fetch.URLFetchRequestOptions = {
+    'method': 'post',
+    'headers': notionApiHeader(),
+    'payload': JSON.stringify(payload)
+  }
+  return options
 }
